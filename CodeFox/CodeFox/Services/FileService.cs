8314a0baa20dcaa6ab14d6bd635ec135f0c021bd
peddilama <pedro16@ru.is>
2017-05-08 15:37:20 +0000

    Nyr listi fyrir file types

--- CodeFox/CodeFox/Services/FileService.cs
+++ CodeFox/CodeFox/Services/FileService.cs
@@ -38,13 +38,6 @@ namespace CodeFox.Services
             return Default;
         }
 
-        public void SaveFile(int FileID, string NewText)
-        {
-            File Tmp = DB.Files.Find(FileID);
-            Tmp.Location = NewText;
-            DB.SaveChanges();
-        }
-
         //Checka a USERNAME
         public void AddFile(AddFilesViewModel Model)
         {
