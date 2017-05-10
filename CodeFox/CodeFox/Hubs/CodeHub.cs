40b1359a97d536f2bbfe383a2ec7a753a23ea930
johannegi <johannegi16@ru.is>
2017-05-10 23:11:26 +0000

    SignalR tveir geta unnið í sama skjali í einu

---
+++ CodeFox/CodeFox/Hubs/CodeHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace CodeFox.Hubs
+{
+    public class CodeHub : Hub
+    {
+        public void LeaveFile(int FileID)
+        {
+            Groups.Remove(Context.ConnectionId, Convert.ToString(FileID));
+        }
+
+        public void JoinFile(int FileID)
+        {
+            Groups.Add(Context.ConnectionId, Convert.ToString(FileID));
+        }
+
+        public void OnChange(object ChangeData, int FileID)
+        {
+            Clients.Group(Convert.ToString(FileID), Context.ConnectionId).OnChange(ChangeData);
+        }
+    }
+}
