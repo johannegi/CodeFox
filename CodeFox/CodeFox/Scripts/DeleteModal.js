f587cdeffe46ba0ad3824fa29096bd5391177fc9
AlexMar16 <alex16@ru.is>
2017-05-12 17:27:30 +0000

    Allt eftir coderules

--- CodeFox/CodeFox/Scripts/DeleteModal.js
+++ CodeFox/CodeFox/Scripts/DeleteModal.js
@@ -1,5 +1,7 @@
-﻿$(document).ready(function () {
-    $('.Delete').on('click', function (e) {
+﻿$(document).ready(function ()
+{
+    $('.Delete').on('click', function ()
+    {
         // We get the ProjectID from the data-id attribute.
         var ProjectID = $(this).data("id");
         // Ajax request sent.
@@ -7,44 +9,51 @@
             url: '/Projects/Delete',
             data: { 'ID': ProjectID },
             method: "GET",
-            success: function (data) {
-                if (data == "") {
+            success: function (Data)
+            {
+                if (Data == "")
+                {
                     $('#ModalText').html('Project Not Found');
                 }
-                else {
+                else
+                {
                     // We build the modal and append it into the tag where .DeleteContent is.
-                    html = "<h2>Delete</h2>";
-                    html += "<h3>Are you sure you want to delete this project?</h3>";
-                    html += "<div>";
-                    html += "<h4>After deleting project can not be restored</h4>";
-                    html += "<hr/>";
-                    html += "<div>";
+                    Html = "<h2>Delete</h2>";
+                    Html += "<h3>Are you sure you want to delete this project?</h3>";
+                    Html += "<div>";
+                    Html += "<h4>After deleting project can not be restored</h4>";
+                    Html += "<hr/>";
+                    Html += "<div>";
 
 
-                    html += " <h2 class='DeleteTitle'>Name:</h2>";
-                    html += "<p class='DeleteText'>" + "&#32;" + data.Name + "<dd>";
+                    Html += " <h2 class='DeleteTitle'>Name:</h2>";
+                    Html += "<p class='DeleteText'>" + "&#32;" + Data.Name + "<dd>";
 
-                    html += " <h2 class='DeleteTitle'>Type:</h2>";
-                    html += "<p class='DeleteText'>" + data.Type + "<dd>";
-                    html += "</div>";
+                    Html += " <h2 class='DeleteTitle'>Type:</h2>";
+                    Html += "<p class='DeleteText'>" + Data.Type + "<dd>";
+                    Html += "</div>";
                    
                     $('.DeleteContent').html('');
-                    $('.DeleteContent').append(html);
+                    $('.DeleteContent').append(Html);
                 }
             }
         });
         // Then we open the modal
         $('#OpenModalDelete').modal();
-        $('.DeleteProject').on('click', function (e) {
+        $('.DeleteProject').on('click', function ()
+        {
             $.ajax({
                 url: '/Projects/Delete',
                 data: { 'ProjectID': ProjectID },
                 method: "POST",
-                success: function (data) {
-                    if (data == "") {
+                success: function (Data)
+                {
+                    if (Data == "")
+                    {
                         $('#ModalText').html('Project Not Found');
                     }
-                    else {
+                    else
+                    {
                         // We redirect the user, to refresh the site so that 
                         // the projects update.
                         window.location = '/Projects';
